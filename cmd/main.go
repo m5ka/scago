1d9740a40ec8f722917e38f81ef071b3bb2a8bc4
Marzena Budek <marzena.budek@outlook.com>
2021-03-29 10:06:27 +0100

    clean up main file before functionality is available

--- cmd/main.go
+++ cmd/main.go
@@ -8,29 +8,38 @@ import (
 )
 
 func main() {
-	inputFile := flag.String("i", "", "file containing a list of input words to be changed")
-	outputFile := flag.String("o", "", "filename for the output of the sound changes")
-	rulesetFile := flag.String("f", "", "file containing a list of rules to be applied to all words")
+	//inputFile := flag.String("i", "", "file containing a list of input words to be changed")
+	//outputFile := flag.String("o", "", "filename for the output of the sound changes")
+	//rulesetFile := flag.String("f", "", "file containing a list of rules to be applied to all words")
 	ruleLiteral := flag.String("r", "", "a single rule to apply to the word(s)")
 	flag.Parse()
 	inputLiteral := flag.Arg(0)
 
 	s := scago.New()
 
-	if *rulesetFile != "" {
-		// TODO: open file, add rules
+	// TODO: allow reading of ruleset file for adding rules to ScagoInstance
+	/*if *rulesetFile != "" {
 		fmt.Println("[DEBUG] This feature hasn't been added yet. Sorry!")
 		return
-	} else if *ruleLiteral != "" {
+	} else*/
+	if *ruleLiteral != "" {
 		s.AddRule(*ruleLiteral)
 	} else {
 		fmt.Println("No rule(s) specified.")
 		return
 	}
 
-	if *inputFile != "" {
+	// TODO: allow reading of input file for processing words to Apply
+	/*if *inputFile != "" {
+		fmt.Println("[DEBUG] This feature hasn't been added yet. Sorry!")
+		return
+	} else if inputLiteral != "" {*/
+	output, err := s.Apply(inputLiteral)
+	//}
 
-	} else if inputLiteral != "" {
-
+	if err != nil {
+		fmt.Println(output)
+	} else {
+		fmt.Println("Something went wrong!", err)
 	}
 }
--- sca.go
+++ sca.go
@@ -1,8 +1,18 @@
 package scago
 
+import "errors"
+
 type ScagoInstance struct {
 	rules      *Rule     // a pointer to the first rule in the list
 	categories *Category // a pointer to the first category in the list
+}
+
+// Applies the ScagoInstance's ruleset to the given word, returning
+// the changed word and whether there were any errors. In case of
+// errors, the returned string may be empty.
+// TODO: implement this functionally
+func (s *ScagoInstance) Apply(lemma string) (string, error) {
+	return "", errors.New("function not yet implemented")
 }
 
 // Returns a new ScagoInstance that can be used to
