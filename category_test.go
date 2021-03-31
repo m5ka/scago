e8964157e7d465737ff2aee65efb55a2293d7c36
Marzena Budek <marzena.budek@outlook.com>
2021-03-31 14:02:47 +0100

    refactor test for NewCategory

--- category_test.go
+++ category_test.go
@@ -1,13 +1,17 @@
 package scago
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCategory(t *testing.T) {
-	testStr := "NewCategory('K', []string{'a', 'b', 'c'})"
+	assert := assert.New(t)
 	got, err := NewCategory("K", []string{"a", "b", "c"})
 	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	} else if got.identifier != "K" || got.pattern != "(a|b|c)" {
-		t.Errorf("%s = {%s, %s}; want {K, '(a|b|c)'}", testStr, got.identifier, got.pattern)
+		t.Fatalf("NewCategory returned error: %s", err)
 	}
+	assert.Equal(got.identifier, "K")
+	assert.Equal(got.pattern, "(a|b|c)")
 }
