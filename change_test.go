9475ebec51692795c5bfc1d3a8db765616839b20
Marzena Budek <marzena.budek@outlook.com>
2021-03-31 13:58:03 +0100

    refactor tests for ParseChange

--- change_test.go
+++ change_test.go
@@ -1,94 +1,71 @@
 package scago
 
-import "testing"
+import (
+	"testing"
 
-func TestParseChangeReplacement(t *testing.T) {
-	testStr := "s.ParseChange('a')"
-	got, err := New().ParseChange("a")
-	wantReplacement := "a"
-	wantMovement := 0
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if got.replacement != wantReplacement {
-		t.Errorf("%s = Replacement: %s; want %s", testStr, got.replacement, wantReplacement)
-	}
-	if got.movement != wantMovement {
-		t.Errorf("%s = Movement: %d; want %d", testStr, got.movement, wantMovement)
-	}
-}
+	"github.com/stretchr/testify/assert"
+)
 
-func TestParseChangeMovement(t *testing.T) {
-	testStr := "s.ParseChange(' @ 6 ')"
-	got, err := New().ParseChange(" @ 6 ")
-	wantReplacement := ""
-	wantMovement := 6
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if got.replacement != wantReplacement {
-		t.Errorf("%s = Replacement: %s; want %s", testStr, got.replacement, wantReplacement)
-	}
-	if got.movement != wantMovement {
-		t.Errorf("%s = Movement: %d; want %d", testStr, got.movement, wantMovement)
-	}
-}
-
-func TestParseChangeMovementNegative(t *testing.T) {
-	testStr := "s.ParseChange('@ -2')"
-	got, err := New().ParseChange("@ -2")
-	wantReplacement := ""
-	wantMovement := -2
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if got.replacement != wantReplacement {
-		t.Errorf("%s = Replacement: %s; want %s", testStr, got.replacement, wantReplacement)
-	}
-	if got.movement != wantMovement {
-		t.Errorf("%s = Movement: %d; want %d", testStr, got.movement, wantMovement)
-	}
-}
-
-func TestParseChangeReplacementAndMovement(t *testing.T) {
-	testStr := "s.ParseChange(' b @ 13 ')"
-	got, err := New().ParseChange(" b @ 13 ")
-	wantReplacement := "b"
-	wantMovement := 13
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if got.replacement != wantReplacement {
-		t.Errorf("%s = Replacement: %s; want %s", testStr, got.replacement, wantReplacement)
-	}
-	if got.movement != wantMovement {
-		t.Errorf("%s = Movement: %d; want %d", testStr, got.movement, wantMovement)
-	}
-}
-
-func TestParseChangeReplacementAndMovementNegative(t *testing.T) {
-	testStr := "s.ParseChange('x@-3')"
-	got, err := New().ParseChange("x@-3")
-	wantReplacement := "x"
-	wantMovement := -3
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if got.replacement != wantReplacement {
-		t.Errorf("%s = Replacement: %s; want %s", testStr, got.replacement, wantReplacement)
-	}
-	if got.movement != wantMovement {
-		t.Errorf("%s = Movement: %d; want %d", testStr, got.movement, wantMovement)
-	}
-}
-
-func TestParseChangeDeletion(t *testing.T) {
-	testStr := "s.ParseChange(' ')"
-	got, err := New().ParseChange(" ")
-	if err != nil {
-		t.Errorf("%s returned error: %s", testStr, err)
-	}
-	if !got.deletion {
-		t.Errorf("%s = deletion: false; want true", testStr)
-	}
+func TestParseChange(t *testing.T) {
+	s := New()
+	t.Run("parse change: replacement", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange("a")
+		if err != nil {
+			t.Fatalf("%s returned error: %s", t.Name(), err)
+		}
+		assert.Equal(got.replacement, "a")
+		assert.Equal(got.movement, 0)
+		assert.False(got.deletion)
+	})
+	t.Run("parse change: movement", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange(" @ 6 ")
+		if err != nil {
+			t.Fatalf("%s returned error: %s", t.Name(), err)
+		}
+		assert.Empty(got.replacement)
+		assert.Equal(got.movement, 6)
+		assert.False(got.deletion)
+	})
+	t.Run("parse change: negative movement", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange("@ -2")
+		if err != nil {
+			t.Fatalf("%s returned err: %s", t.Name(), err)
+		}
+		assert.Empty(got.replacement)
+		assert.Equal(got.movement, -2)
+		assert.False(got.deletion)
+	})
+	t.Run("parse change: replacement and movement", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange("b @ 13 ")
+		if err != nil {
+			t.Fatalf("%s returned err: %s", t.Name(), err)
+		}
+		assert.Equal(got.replacement, "b")
+		assert.Equal(got.movement, 13)
+		assert.False(got.deletion)
+	})
+	t.Run("parse change: replacement and negative movement", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange("x@-3")
+		if err != nil {
+			t.Fatalf("%s returned err: %s", t.Name(), err)
+		}
+		assert.Equal(got.replacement, "x")
+		assert.Equal(got.movement, -3)
+		assert.False(got.deletion)
+	})
+	t.Run("parse change: deletion", func(t *testing.T) {
+		assert := assert.New(t)
+		got, err := s.ParseChange(" ")
+		if err != nil {
+			t.Fatalf("%s returned err: %s", t.Name(), err)
+		}
+		assert.Empty(got.replacement)
+		assert.Empty(got.movement)
+		assert.True(got.deletion)
+	})
 }
